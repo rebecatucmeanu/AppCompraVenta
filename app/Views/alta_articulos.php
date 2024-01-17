0c9119fcd62be501ae9d494400ca69435c69ce0e
rebecatucmeanu <atp0011@alu.medac.es>
2024-01-17 09:14:08 +0100

    modificaciones alta_articulos.php

--- app/Views/alta_articulos.php
+++ app/Views/alta_articulos.php
@@ -14,14 +14,37 @@
 </head>
 <body>
     <h1>Alta de artículos de ropa</h1>
+    <div>
+        <?php
+            if(isset($guardado))
+            {
+            ?>
+                <p>Los datos se han guardado correctamente.</p>
+                <a href="<?php echo base_url()?>">Volver</a>
+            <?php    
+            }
+        ?>
+    </div>
     <form method="post" action="<?php echo base_url('articulos/guardar') ?>" enctype="multipart/form-data">
-        <label for="nombre">Nombre</label>
-        <input type="text" name="nombre"/><br>
-        <label for="nombre">Marca</label>
-        <input type="text" name="marca"/><br>
-        <label for="nombre">Precio</label>
-        <input type="text" name="precio"/><br>
-        <input type="submit" name="Enviar">
+        <div> 
+            <label for="nombre">Nombre</label>
+            <input type="text" name="nombre"/><br>
+        </div>
+        <div>
+            <label for="nombre">Marca</label>
+            <input type="text" name="marca"/><br>
+        </div>
+        <div>
+            <label for="nombre">Precio</label>
+            <input type="text" name="precio"/><br>
+        </div>
+        <div>
+            <label for="imagen">Imagen</label>
+            <input type="file" name="imagen">
+        </div>
+        <div>
+            <input type="submit" name="Enviar">
+        </div>
     </form>
 </body>
 </html>
