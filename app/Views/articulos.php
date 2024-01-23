6a1f5370806dc8ff831d198312cfabb80b66bb01
rebecatucmeanu <atp0011@alu.medac.es>
2024-01-23 09:24:57 +0100

    intento tarjeta bootstrap

--- app/Views/articulos.php
+++ app/Views/articulos.php
@@ -5,16 +5,25 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Artículos</title>
-    <style>
-        h1, p
-        {
-            padding-left: 25px;
-        }
-    </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
 <body>
-    <h1>Gabardina azul marino</h1>
-    <p>Talla: S</p>
-    <p>Marca: Easy Wear</p>
+    <div class="container-fluid bg-trasparent my-4 p-3" style="position: relative;"> 
+        <div class="row row-cols-1 row-cols-xs-2 row-cols-sm-2 row-cols-lg-4 g-3"> 
+            <div class="col"> 
+                <div class="card h-100 shadow-sm"> 
+                    <img src="" class="card-img-top" alt="..."> 
+                    <div class="card-body"> 
+                        <div class="clearfix mb-3"> 
+                            <span class="float-start badge rounded-pill bg-primary">Ejemplo tarjeta Bootstrap</span> <span class="float-end price-hp">20&euro;</span> 
+                        </div> 
+                        <h5 class="card-title">Ejemplo</h5> 
+                        <div class="text-center my-4"> <a href="#" class="btn btn-warning">Ver artículo</a></div> 
+                    </div> 
+                </div> 
+            </div> 
+        </div>
+    </div>
 </body>
 </html>
