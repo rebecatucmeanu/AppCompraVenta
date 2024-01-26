8a72a7f4cedf9d987135b8dc7cac990f2b009927
rebecatucmeanu <atp0011@alu.medac.es>
2024-01-26 04:39:06 +0100

    añadir botón agrergar artículos

--- app/Views/templates/usuario_guardado.php
+++ app/Views/templates/usuario_guardado.php
@@ -1,4 +1,5 @@
         <h1>Usuario creado con éxito.</h1>
-        <a href="<?php echo base_url('usuarios/mostrar_login') ?>"><button>Iniciar sesión</button></a>    
+        <a href="<?php echo base_url('usuarios/mostrar_login') ?>"><button>Iniciar sesión</button></a>  
+        <a href="<?php echo base_url('articulos/crear') ?>"><button>Dar de alta un artículo</button></a>   
     </body>
 </html>
